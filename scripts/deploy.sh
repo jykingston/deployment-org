86ae38e0bbc930fb08fe207cba23a5e6c6e04a0c
Jy Kingston <jy@buildkite.com>
2025-03-05 10:59:14 +1100

    Fix the pipelines

---
+++ scripts/deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# Set up a variable to hold the meta-data from your block step
+RELEASE_NAME="$(buildkite-agent meta-data get "release-name")"
+RELEASE_NAME="$(buildkite-agent meta-data get "release-type")"
+
+# Create a pipeline with your trigger step
+PIPELINE="steps:
+- trigger: \"eft-sure-deployment-$RELEASE_TYPE\"
+    label: \"Trigger deploy to $RELEASE_NAME\"
+    build:
+      meta_data:
+        release-type: $RELEASE_TYPE
+        release-name: $RELEASE_TYPE
+"
+
+# Upload the new pipeline and add it to the current build
+echo "$PIPELINE" | buildkite-agent pipeline upload
