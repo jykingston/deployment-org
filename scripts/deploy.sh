a382755cc2a0aa80285ab839e26f3a90fe9cac17
Jy Kingston <jy@buildkite.com>
2025-03-05 11:46:13 +1100

    Update naming

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,13 +3,12 @@
 set -euo pipefail
 
 # Set up a variable to hold the meta-data from your block step
-RELEASE_NAME="$(buildkite-agent meta-data get "release-name")"
 RELEASE_TYPE="$(buildkite-agent meta-data get "release-type")"
 
 # Create a pipeline with your trigger step
 PIPELINE="steps:
   - trigger: \"eft-sure-$RELEASE_TYPE-deployment\"
-    label: \"Trigger deploy to $RELEASE_NAME\"
+    label: \"Trigger deploy to $RELEASE_TYPE\"
     build:
       meta_data:
         release-type: $RELEASE_TYPE
